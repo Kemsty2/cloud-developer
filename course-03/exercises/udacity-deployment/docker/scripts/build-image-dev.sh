da4def7c731c5511f92c6aa946d846c2aa3fdda2
Kemgne Moyo Steeve Aymard <kemgnemoyosteeveaymard@gmail.com>
2019-12-20 23:43:40 +0100

    mod travis file

--- course-03/exercises/udacity-deployment/docker/scripts/build-image-dev.sh
+++ course-03/exercises/udacity-deployment/docker/scripts/build-image-dev.sh
@@ -6,8 +6,8 @@ set -ev
 docker-compose -f $PWD/course-03/exercises/udacity-deployment/docker/docker-compose-build-dev.yaml build
 
 #   login to docker registry
-docker login --password $DOCKER_REGISTRY_PASSWORD -username $DOCKER_REGISTRY_USERNAME
-
+#docker login --password $DOCKER_REGISTRY_PASSWORD -username $DOCKER_REGISTRY_USERNAME
+echo "$DOCKER_REGISTRY_PASSWORD" | docker login -u "$DOCKER_REGISTRY_USERNAME" --password-stdin
 #   push all the images of my project to docker registry
 docker push kemsty2/frontend:dev
 docker push kemsty2/reverseproxy:dev
--- course-03/exercises/udacity-deployment/docker/scripts/build-image-prod.sh
+++ course-03/exercises/udacity-deployment/docker/scripts/build-image-prod.sh
@@ -6,7 +6,7 @@ set -ev
 docker-compose -f $PWD/course-03/exercises/udacity-deployment/docker/docker-compose-build-prod.yaml build
 
 #   login to docker registry
-docker login --password $DOCKER_REGISTRY_PASSWORD -username $DOCKER_REGISTRY_USERNAME
+echo "$DOCKER_REGISTRY_PASSWORD" | docker login -u "$DOCKER_REGISTRY_USERNAME" --password-stdin
 
 #   push all the images of my project to docker registry
 docker push kemsty2/frontend:prod
