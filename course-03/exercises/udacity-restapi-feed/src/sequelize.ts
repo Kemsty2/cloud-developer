2307e4fa5248216ac507885b073186e43e1b90c6
Kemgne Moyo Steeve Aymard <kemgnemoyosteeveaymard@gmail.com>
2019-12-24 21:51:57 +0100

    try fix bugs

--- course-03/exercises/udacity-restapi-feed/src/sequelize.ts
+++ course-03/exercises/udacity-restapi-feed/src/sequelize.ts
@@ -5,13 +5,7 @@ import { config } from './config/config';
 const c = config.dev;
 
 // Instantiate new Sequelize instance!
-export const sequelize = new Sequelize({
-  "username": c.username,
-  "password": c.password,
-  "database": c.database,
-  "host":     c.host,
 
-  dialect: 'postgres',
-  storage: ':memory:',
-});
+export const sequelize = new Sequelize(
+  `postgres://${c.username}:${c.password}@${c.host}:5432/${c.database}`);
 
--- course-03/exercises/udacity-restapi-user/src/sequelize.ts
+++ course-03/exercises/udacity-restapi-user/src/sequelize.ts
@@ -1,17 +1,9 @@
-import {Sequelize} from 'sequelize-typescript';
-import { config } from './config/config';
-
+import { Sequelize } from "sequelize-typescript";
+import { config } from "./config/config";
 
 const c = config.dev;
 
 // Instantiate new Sequelize instance!
-export const sequelize = new Sequelize({
-  "username": c.username,
-  "password": c.password,
-  "database": c.database,
-  "host":     c.host,
 
-  dialect: 'postgres',
-  storage: ':memory:',
-});
-
+export const sequelize = new Sequelize(
+  `postgres://${c.username}:${c.password}@${c.host}:5432/${c.database}`);
